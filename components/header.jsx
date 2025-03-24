c7e9e55176c3b38f69be221237a9c9735ae64b6a
muskan popli <muskan1popli@gmail.com>
2025-03-24 16:14:47 +0530

    Add user authentication flow

--- components/header.jsx
+++ components/header.jsx
@@ -1,16 +1,9 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import React from "react";
-// import { Button } from "./ui/button";
-// import { PenBox, LayoutDashboard } from "lucide-react";
-// import Link from "next/link";
-//import { checkUser } from "@/lib/checkUser";
-//import Image from "next/image";
 
 const Header = async () => {
-    //   await checkUser();
-
     return (
-        <div>
+        <div className="fixed top-0">
             <SignedOut>
                 <SignInButton />
             </SignedOut>
