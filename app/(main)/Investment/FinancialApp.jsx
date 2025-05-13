acb1d3f0e3ac3971014f9e39bd3bb3468e43b6a2
Dipesh4795 <dintelgupta31702@gmail.com>
2025-05-13 22:50:16 +0530

    add

--- app/(main)/Investment/FinancialApp.jsx
+++ app/(main)/Investment/FinancialApp.jsx
@@ -35,7 +35,7 @@ export default function FinancialApp() {
           portfolioSuggestion: data.portfolioSuggestion,
         });
       } else {
-        a;
+        console.log("main");
       }
     } catch (err) {
       console.error(err);
